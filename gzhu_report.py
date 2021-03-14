c0fe401dc8c0afa8d9e14dcbf42f36cdb4a09cee
veteran233 <a1328906205@live.com>
2021-03-14 23:57:34 +0800

    Add files via upload

---
+++ gzhu_report.py
@@ -0,0 +1,33 @@
+from playwright.sync_api import sync_playwright
+import time
+
+
+def run(playwright):
+    browser = playwright.chromium.launch(headless=False)
+
+    context = browser.new_context()
+
+    page_cookies =  # Copy Here
+
+    context.add_cookies(page_cookies)
+
+    page = context.new_page()
+
+    page.goto("http://yqtb.gzhu.edu.cn/infoplus/form/XNYQSB/start")
+    time.sleep(3)
+
+    page.check("input[name=\"fieldCNS\"]")
+    time.sleep(3)
+
+    page.click("a:has-text(\"提交\")")
+    time.sleep(3)
+
+    page.click("text=确定")
+    time.sleep(3)
+
+    context.close()
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
